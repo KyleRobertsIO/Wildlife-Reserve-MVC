c71823e15232cc8a40e616335c90f1c3213c762d
Kyle <kkroberts1635@gmail.com>
2019-07-09 08:37:19 -0400

    Homepage added

---
+++ Reserve API/Content/js/navigation.js
@@ -0,0 +1,19 @@
+﻿function mobileDropdownToggle() {
+    let menu = document.getElementById('mobile-dropdown-menu');
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
+
+function closeMobileDropdown() {
+    let menu = document.getElementById('mobile-dropdown-menu');
+    menu.style.display = 'none';
+}
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth > 450) {
+        closeMobileDropdown();
+    }
+});
