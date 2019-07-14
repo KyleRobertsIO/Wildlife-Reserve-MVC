bce2fabf41579815cfa281f6c9277268dd494f6b
Kyle Roberts <kkroberts1635@gmail.com>
2019-07-14 12:59:47 -0400

    AJAX for animals page added and data is stored in local storage

---
+++ Reserve API/Content/js/animals_request.js
@@ -0,0 +1,34 @@
+﻿fetch('/api/animals')
+    .then(function(response){
+        return response.json();
+    })
+    .then(function (myJson) {
+        let animals = JSON.stringify(myJson);
+        localStorage.setItem('animals', animals);
+        createAnimalCards();
+    });
+
+function createAnimalCards() {
+    let data = localStorage.getItem('animals');
+    if (data != null) {
+        let animals = JSON.parse(data);
+        console.log(animals);
+        let template = document.getElementById("animal-card-template").content;
+        let gridContainer = document.getElementById("animal-card-list");
+        animals.forEach((animal) => {
+            let clone = template.cloneNode(true);
+            clone.querySelector('.animal-card-container').id = animal.SpeciesID;
+            let filePath = `../Content/assets/images/animal_images/${animal.Photos[0].FileName}`;
+            clone.querySelector('.card-image').setAttribute("src", filePath);
+            clone.querySelector('.card-image').setAttribute("alt", `${animal.Species} Photo`);
+            clone.querySelector('.animal-card-species').innerText = animal.Species;
+            clone.querySelector('.animal-card-desc').innerText = animal.Description.DescriptionShort;
+            gridContainer.appendChild(clone);
+        });
+    }
+}
+
+function displayAnimalPage() {
+    console.log("Open Display");
+
+}
